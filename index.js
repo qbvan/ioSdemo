076f10b2268b324daa0d0612c3f68d1206e41108
qbvan <qbvan.hau.94@gmail.com>
2018-06-18 16:01:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
-    console.log("hello")
-    // change new content
-});
+var express = require('express');
+var bodyParser = require('body-parser);
+var path = require('path')
+var app = express();
+var port = process.PORT.env || 3000;
+
+app.listen(port, () => {
+    console.log("connect to server on port " + port);
+})
